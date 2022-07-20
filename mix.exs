828d3798b1c95dfc846cc0aafffe9c16a50624ac
okothkongo <okothkongo@gmail.com>
2022-07-20 15:37:41 +0300

    setup credo

--- mix.exs
+++ mix.exs
@@ -48,7 +48,8 @@ defmodule ElixirSip.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.6"}
     ]
   end
 
@@ -64,7 +65,13 @@ defmodule ElixirSip.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.deploy": ["esbuild default --minify", "phx.digest"]
+      "assets.deploy": ["esbuild default --minify", "phx.digest"],
+      lint: [
+        "format",
+        "format --check-formatted",
+        "compile --warnings-as-errors --force",
+        "credo --strict"
+      ]
     ]
   end
 end
